62f88ed5b810fbfffb0d1b371ab409cb5ebf6cbb
Shadrack Sithole <shadrackdrack@gmail.com>
2024-04-13 14:35:45 +0200

    My initial commit

---
+++ src/main/java/com/sirack/fullstactbankend/FullstactBankendApplication.java
@@ -0,0 +1,13 @@
+package com.sirack.fullstactbankend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstactBankendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstactBankendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirack/fullstactbankend/FullstactBankendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirack.fullstactbankend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstactBankendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
